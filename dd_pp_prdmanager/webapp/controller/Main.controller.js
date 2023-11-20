e0f3e1e4c2bb82c0cbde73377d92d6440e099b44
static447 <star8609@naver.com>
2023-11-20 18:22:11 +0900

    생산현황관리

--- dd_pp_prdmanager/webapp/controller/Main.controller.js
+++ dd_pp_prdmanager/webapp/controller/Main.controller.js
@@ -2,12 +2,14 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
-    "sap/ui/core/format/DateFormat"
+    "sap/ui/core/format/DateFormat",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, Filter, DateFormat) {
+    function (Controller, JSONModel, Filter, DateFormat, MessageToast, Fragment) {
         "use strict";
 
         return Controller.extend("ddppprdmanager.controller.Main", {
@@ -17,31 +19,37 @@ sap.ui.define([
 
                 this.getView().setModel(oModel, 'tree');
 
-                var monthModel = new JSONModel({
+                var oData = new JSONModel({
                     Prdmanage : [],
                     Chart : []
                 });
-                var oModelData = this.getView().setModel(monthModel, "main");
+                var oModelData = this.getView().setModel(oData, "main");
                 
             },
 
-            onSearch(){
-                // 트리 데이터 찾기
-                let index = this.byId("TreeTableBasic").getSelectedIndex(),
+            onSelectChange(oEvent) {
+                
+                // let index = this.byId("TreeTableBasic").getSelectedIndex(),
+
+                let index = oEvent.getParameters().rowIndex,
                     sPath = this.byId("TreeTableBasic").getContextByIndex(index).sPath,
                     oData = this.getView().getModel("tree").getProperty(sPath),
                     aFilter = [];
 
-                let combo = this.byId("comboid").getValue();
+                var oModelData = this.getView().getModel("main");
 
-                if(combo){
-                    let plantFilter = new Filter('Plcode', 'EQ', combo);
+                if(index && oData.id == 'year'){
+                    let styear = new Date(oData.name + '-01-01');
+                    let edyear = new Date(oData.name + '-12-31');
 
-                    aFilter.push(plantFilter);
+                    let yearFilter = new Filter('Ppstdat', 'BT', styear, edyear);
 
+                    aFilter.push(yearFilter);
+                    this.byId("idTitle").setFooter(oData.name + "년 생산량")
                 }
+                
 
-                if(index){
+                if(index && oData.month){
                     // let month = String(oData.year) + "-" + String(oData.month);
 
                     // 이유는 모르겠으나 월은 해당 숫자에 +1 에 해당하는 값이 나온다
@@ -51,29 +59,75 @@ sap.ui.define([
                     let monthFilter = new Filter('Ppstdat', 'BT', stdate, eddate);
 
                     aFilter.push(monthFilter);
+                    this.byId("idTitle").setFooter((index-1) + "월 생산량")
                 };
 
-                var oModelData = this.getView().getModel("main");
+                
 
+                
                 this.getView().getModel().read("/PrdmanagerSet" , {
                     filters : aFilter,
                     success : function(oReturn){
                         oModelData.setProperty("/Prdmanage", oReturn.results);
 
-                        debugger;
-                        let temp = [];
-                        let chartStatus = oModelData.getProperty("/Prdmanage");
-                        for(var i = 0; i < chartStatus.length; i++){
-                            if(chartStatus[i].Status === "3"){
-                                temp.push(chartStatus[i]);
-                            }
-                        };
+                        let tempChart = [];
+                        let prmData = oModelData.getProperty("/Prdmanage");
 
-                        oModelData.setProperty("/Chart", temp);
-                    }
+                        var totalCnt =  prmData.length;
+                        var productionCnt = 0, waitCnt = 0, completeCnt = 0, totalQuan = 0;
+                        var noQuan = 0, deQuan = 0;
+
+                        if(prmData.length == 0){
+                            MessageToast.show("데이터가 존재하지 않습니다.");
+                        }
+                        else{
+                            
+                            for(var i = 0; i < prmData.length; i++){
+                                if(prmData[i].Status){
+                                    switch(prmData[i].Status){
+                                        case "2":
+                                            waitCnt = waitCnt + 1
+                                            break;
+                                        // 최종 완료 데이터 카운트
+                                        case "3":
+                                            completeCnt = completeCnt + 1
+                                            totalQuan = totalQuan + Number(prmData[i].Quan)
+                                            noQuan = noQuan + Number(prmData[i].Noquan);
+                                            deQuan = deQuan + Number(prmData[i].Dequan);
+                                            tempChart.push(prmData[i])
+                                            break;
+                                    }
+                                }
+                                else{
+                                    // 생산 중 데이터 카운트
+                                    productionCnt = productionCnt + 1
+                                }
+                        
+                            };
+
+                        }
+                        
+                        // 객체로 count 변경 -> 실패
+                        // tempCount.push({totalCnt, productionCnt, waitCnt, completeCnt})      
+                        // oModelData.setProperty("/Count", tempCount);
+
+                        oModelData.setProperty("/Chart", tempChart);
+
+                        let percentage = (( deQuan / noQuan ) * 100).toFixed(1);
+                        let quanLength = String(totalQuan).length;
+                        debugger;
+                        this.getView().byId("idMicro").setPercentage(Number(percentage));
+
+
+                        this.byId("idnc1").setTruncateValueTo(quanLength);
+                        this.byId("idnc1").setValue(totalQuan);
+                        this.getView().byId("idtabTotal").setCount(totalCnt);
+                        this.getView().byId("idtabProduction").setCount(productionCnt);
+                        this.getView().byId("idtabWait").setCount(waitCnt);
+                        this.getView().byId("idtabComplete").setCount(completeCnt);
+                    }.bind(this)
 
                 });
-                
             },
 
             // 포매터 함수들
@@ -141,6 +195,12 @@ sap.ui.define([
                 aFilter.push(oFilter);
 
                 this.byId("ProductTable").getBinding("rows").filter(aFilter);
+            },
+
+            onTile: function(oEvent){
+
+                this.byId("idpopover").openBy(oEvent.getSource());
+
             }
         });
     });
